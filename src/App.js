980f1c32e10ca0f7ea9318bae5d3d0fcfdc22025
lauradevuono <lauradevuono@gmail.com>
2023-02-25 15:09:29 -0500

    install dnd, create draggable context, create first column + hardcoded tasks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,80 @@
 import './App.css';
+import { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { v4 as uuid } from "uuid";
+
+const itemsFromBackend = [
+  { id: uuid(), content: 'First task' },
+  { id: uuid(), content: 'Second task' }
+];
+
+const columnsFromBackend = {
+  [uuid()]: {
+    name: 'Todo',
+    items: itemsFromBackend
+  }
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination)
+    return;
+  const { source, destination } = result;
+}
 
 function App() {
+  const [columns, setColumns] = useState(columnsFromBackend);
   return (
     <div className="App">
+      <DragDropContext onDragEnd={result => console.log(result)}>
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <Droppable droppableId={id} key={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500
+                    }}
+                  >
+                    {column.items.map((item, index) => {
+                      return (
+                        <Draggable key={item.id} draggableId={item.id} index={index}>
+                          {(provided, snapshot) => {
+                            return (
+                              <div ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                style={{
+                                  userSelect: 'none',
+                                  padding: 16,
+                                  margin: '0 0 8px 0',
+                                  minHeight: '50px',
+                                  backgroundColor: snapshot.isdragging ? '#263b4a' : '#456c86',
+                                  color: 'white',
+                                  ...provided.draggableProps.style
+                                }}
+                              >
+                                {item.content}
+                              </div>
+                            )
+                          }}
+                        </Draggable>
+                      )
+                    })}
+                    {provided.placeholder}
+                  </div>
+                );
+              }}
+            </Droppable>
+
+          )
+        })}
+      </DragDropContext>
 
     </div>
   );
